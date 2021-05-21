5fcd49a11ea67082d8a1a2f2ba423e7734412504
melissa-stott <1princessmelissa@gmail.com>
2021-05-20 21:06:22 -0700

    added auth0 dependencies and set up endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var checkJwt = require('../utilities/authorization/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('respond with a resource');
+});
+
+router.get('/private', checkJwt, function(req, res) {
+	res.json({
+		message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+	});
 });
 
 module.exports = router;
