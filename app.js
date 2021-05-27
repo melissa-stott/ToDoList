d46406b6e1f30f3082840656caaad9a2cb34b2fe
melissa-stott <1princessmelissa@gmail.com>
2021-05-27 05:55:53 -0700

    added connection to mongodb

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +18,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Mongo Connection
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/ToDoList', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+});
+
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true
+	})
+);
 
 module.exports = app;
